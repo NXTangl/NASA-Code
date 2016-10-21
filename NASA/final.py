dc630756c5ab7a844d316fb96a0a1bc80e2aa6ed
NXTangl <NXTangl@gmail.com>
2016-10-21 18:03:04 -0500

    Commenting.
    Added documentation to the file.

--- NASA/final.py
+++ NASA/final.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
+# I gotta ask...why tabs? Python standard is 4 spaces indent.
+
+
+
 import socket
 import RPi.GPIO as GPIO
 PORT=9500
@@ -25,49 +29,69 @@ Auger = GPIO.PWM(13, 50)
 Slide = GPIO.PWM(19, 50)
 Tilt = GPIO.PWM(26, 50)
 Convey = GPIO.PWM(21,50)
+
+# initialize PWM drivers.
+# PWM 0%-100% duty cycle is interpreted
+# as full reverse to full forward.
+# Thus, 50% = stopped.
 Drive.start(50)
 Steer.start(50)
 Auger.start(50)
 Slide.start(50)
 Tilt.start(50)
+
+# FIXME: Is Convey supposed to be initialized to 0?
 Convey.start(0)
 
+# These count the number of toggle commands.
+# It's a rather silly way to do things, toggle commands
+# are easy to implement client-side.
 augerCount = 1
 tiltCount = 1
 conveyorCount = 1
+
+
+#main loop.
 while True:
+	# Grab the packet.
 	info = conn.recv(6).decode()
-	if info[:2] == 'DR':
+	
+	# This is a value selection statement.
+	# Or it would be, if python had one.
+	
+	if info[:2] == 'DR': # Drive motor change command.
 		pwm = float(info[2:])
 		Drive.ChangeDutyCycle(pwm)
-	if info[:2] == 'ST':
+	if info[:2] == 'ST': # Steering motor change command.
 		pwm = float(info[2:])
 		Steer.ChangeDutyCycle(pwm)
-	if info[:2] == 'AU':
+	if info[:2] == 'AU': # Augur motor change command.
+		# This is a really silly way of doing it! Not to mention it causes problems if
+		# the socket ever fails and the laptop and robot get out of sync.
 		pwm = float(info[2:])
-		augerCount = augerCount +1
-		if augerCount % 2 == 0:
+		augerCount += 1
+		if augerCount % 2 == 0: # Update auger every first 'AU' command.
 			Auger.ChangeDutyCycle(pwm)
-		else:
+		else: # Stop auger every second 'AU' command.
 			Auger.ChangeDutyCycle(50)				
-	if info[:2] == 'SL':
+	if info[:2] == 'SL': # Slide motor change command.
 		pwm = float(info[2:])
 		Slide.ChangeDutyCycle(pwm)
 		
-	if info[:2] == 'TI':
-		tiltCount = tiltCount + 1
+	if info[:2] == 'TI': # Tilt motor change command.
+		tiltCount += 1
 		pwm = float(info[2:])
-		if tiltCount % 2 == 0:
+		if tiltCount % 2 == 0: # Update every first message.
 			Tilt.ChangeDutyCycle(pwm)
-		else:
+		else: # Stop every second message.
 			Tilt.ChangeDutyCycle(50)
 			
 			
 	if info[:2] == 'CO':
-		conveyorCount = conveyorCount + 1
-		if conveyorCount %2 == 1:
+		conveyorCount += 1
+		if conveyorCount %2 == 1: # Set to forward@80% every SECOND message.
 			Convey.ChangeDutyCycle(90)
-		else:
+		else: # set to neutral every FIRST message.
 			Convey.ChangeDutyCycle(50)
-s.close()
-exit(0)
+s.close() # The loop never actually ends normally (no break), so this close does nothing.
+exit(0) # Ditto this exit.
